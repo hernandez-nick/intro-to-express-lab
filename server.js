439c28b881ae7e091399044bf26dc0bcac1ce9f0
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-02-01 19:36:48 -0800

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+  const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+
 const app = express();
 let roll;
 let number;
@@ -18,11 +25,21 @@ app.get('/roll/:number', (req, res) => {
     if (roll !== number) {
         console.log('You must specify a number');
     } else {
-        const number = parseInt(req.params.number);
-        res.send(`You rolled a ${number}`);
+        number = parseInt(req.params.number, 20);
+        roll = Math.floor(Math.random() * number);
+        res.send(`You rolled a ${roll}`);        
     }
 });
 
+app.get('/collectible/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const item = collectibles[id];
+    if (item) {
+        res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+    } else {
+        res.send('This item is not yet in stock. Check back soon!');
+    }
+});
 
 
 app.listen(3000, () => {
