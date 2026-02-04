eb50aa1ad935601008c346713434b41d61f4bea9
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-02-03 21:06:09 -0800

    final commit

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 
-  const collectibles = [
+const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
+];
 
-  const shoes = [
-      { name: "Birkenstocks", price: 50, type: "sandal" },
-      { name: "Air Jordans", price: 500, type: "sneaker" },
-      { name: "Air Mahomeses", price: 501, type: "sneaker" },
-      { name: "Utility Boots", price: 20, type: "boot" },
-      { name: "Velcro Sandals", price: 15, type: "sandal" },
-      { name: "Jet Boots", price: 1000, type: "boot" },
-      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
-  ];
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
 
 const app = express();
 
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Intro to Express Lab!');
+    res.send('Welcome to Intro to Express Lab!');
 });
 
 
@@ -51,12 +51,31 @@ app.get('/collectible/:index', (req, res) => {
     }
 });
 
-app.get('/shoes/query', (req, res) => {
-    const type = req.query.type;
-};
+
+app.get('/shoes', (req, res) => {
+    let filteredShoes = shoes;
+
+    const minPrice = parseFloat(req.query['min-price']);
+    const maxPrice = parseFloat(req.query['max-price']);
+    const type = req.query['type'];
+
+    if (!isNaN(minPrice)) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+    }
+
+    if (!isNaN(maxPrice)) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+    }
+
+    if (type) {
+        filteredShoes = filteredShoes.filter(shoe => shoe.type.toLowerCase() === type.toLowerCase());
+    }
+
+    res.json(filteredShoes);
+});
 
 
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
