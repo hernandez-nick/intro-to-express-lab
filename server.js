9985d429642b6065604db4c3cfb422115656c44e
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-02-03 20:09:17 -0800

    made corrections that Avisa pointed out

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const express = require('express');
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
 
+  const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
+
 
 const app = express();
-let roll;
-let number;
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Intro to Express Lab!');
@@ -21,25 +30,31 @@ app.get('/greetings/:username', (req, res) => {
     res.send(`What a delight it is to see you once more, ${username}!`);
     });
 
+
 app.get('/roll/:number', (req, res) => {
-    if (roll !== number) {
-        console.log('You must specify a number');
-    } else {
-        number = parseInt(req.params.number, 20);
-        roll = Math.floor(Math.random() * number);
-        res.send(`You rolled a ${roll}`);        
+    const number = Number(req.params.number);
+    if (isNaN(number)) {
+        return res.send('You must specify a number.');
     }
+    const roll = Math.floor(Math.random() * (number + 1));
+    res.send(`You rolled a ${roll}`);
 });
 
-app.get('/collectible/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const item = collectibles[id];
+
+app.get('/collectible/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
+    const item = collectibles[index];
     if (item) {
-        res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+        res.send(`So, you want the ${item.name}? For $${item.price}, it can be yours!`);
     } else {
         res.send('This item is not yet in stock. Check back soon!');
     }
 });
+
+app.get('/shoes/query', (req, res) => {
+    const type = req.query.type;
+};
+
 
 
 app.listen(3000, () => {
